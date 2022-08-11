dbc4803890c16b8c8af17b8495e2074aeecfb709
Muhammed <muhammed@sendme.ng>
2022-08-11 22:55:29 +0100

    feat: setup app views

--- drone/views.py
+++ drone/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Drone, Medication
+from .serializers import DroneSerializer, MedicationSerializer
 
 # Create your views here.
+
+class DroneViewSet(viewsets.ModelViewSet):
+    """API endpoint for the drone"""
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class MedicationViewSet(viewsets.ModelViewSet):
+    """API endpoint for the medication"""
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
