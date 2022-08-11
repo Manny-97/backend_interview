38d32604886185e977692e4afdb9d94be1aff808
Muhammed <muhammed@sendme.ng>
2022-08-11 22:21:51 +0100

    feat: setup image folder

--- drone/admin.py
+++ drone/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from drone.models import Drone
+from drone.models import Drone, Medication
 
 # Register your models here.
 admin.site.register(Drone)
+admin.site.register(Medication)
