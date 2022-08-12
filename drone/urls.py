e75cff5cdd48a6e9b125efcbf8b453d556c90013
Muhammed <muhammed@sendme.ng>
2022-08-12 11:06:05 +0100

    feat:

--- drone/urls.py
+++ drone/urls.py
@@ -5,7 +5,6 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Backend')
 
-
 router = routers.DefaultRouter()
 router.register(r'drones', views.DroneViewSet)
 router.register(r'medication', views.MedicationViewSet)
