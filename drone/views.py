7243700464d396adf176bac9d2d807c80cbfa5ef
Muhammed <muhammed@sendme.ng>
2022-08-15 12:42:19 +0100

    feat: add available and battery capacity views

--- drone/views.py
+++ drone/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
-from .models import Drone, Medication
+from django.http.response import Http404
+from .models import Drone, Medication, LoadInformation
 from .serializers import DroneSerializer, MedicationSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -9,7 +12,29 @@ class DroneViewSet(viewsets.ModelViewSet):
     """API endpoint for the drone"""
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
+    allowed_methods = ['get', 'post', 'patch', 'put']
     # permission_classes = [permissions.IsAuthenticated]
+
+    @action(methods=['get'], detail=False, url_name='available')
+    def available(self, request, *args, **kwargs):
+        """ """
+        filtered = Drone.objects.filter(
+            battery_capacity__gt=25, state__exact='IDLE'
+        )
+        serializer = self.get_serializer(filtered, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_name='battery')
+    def battery(self, requst, *args, **kwargs):
+        """ """
+        drone = self.get_object()
+        if not drone:
+            raise Http404
+        serializer = self.get_serializer(drone)
+        response_data = {
+            'battery_capacity': f"{serializer.data['battery_capacity']}%"
+        }
+        return response_data
 
 
 class MedicationViewSet(viewsets.ModelViewSet):
